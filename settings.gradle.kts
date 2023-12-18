83289f3fd31de0f0194dee791f6b79ac16c1997c
fatima zahra <fatimazahra.nadi@usmba.ac.ma>
2023-12-18 18:23:42 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CalculatriceTp1"
+include(":app")
+ 
